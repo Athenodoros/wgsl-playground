a63cfa04d23b7ea399afc321eecc006b3a3ec0c8
Henry Stoke <github@hmstoke.com>
2026-09-11 02:31:13 +0000

    Go back to old indenting

--- src/app/components/AppNavbar.tsx
+++ src/app/components/AppNavbar.tsx
@@ -1,32 +1,30 @@
 import { AnchorButton, Button, Classes, Drawer, Navbar } from "@blueprintjs/core";
 import { useState } from "react";
 
-export const AppNavbar: React.FC = () => {
-    return (
-        <Navbar fixedToTop={true} className="bp5-dark z-10">
-            <Navbar.Group align="left">
-                <Navbar.Heading>WGSL Playground</Navbar.Heading>
-            </Navbar.Group>
-            <Navbar.Group align="right" className="gap-3">
-                <AnchorButton
-                    icon="share"
-                    text="WGSL Spec"
-                    href="https://www.w3.org/TR/WGSL/"
-                    variant="outlined"
-                    target="_blank"
-                />
-                <AnchorButton
-                    icon="git-new-branch"
-                    text="View Source"
-                    href="https://github.com/Athenodoros/wgsl-playground"
-                    variant="outlined"
-                    target="_blank"
-                />
-                <HelpNavbarButton />
-            </Navbar.Group>
-        </Navbar>
-    );
-};
+export const AppNavbar: React.FC = () => (
+    <Navbar fixedToTop={true} className="bp5-dark z-10">
+        <Navbar.Group align="left">
+            <Navbar.Heading>WGSL Playground</Navbar.Heading>
+        </Navbar.Group>
+        <Navbar.Group align="right" className="gap-3">
+            <AnchorButton
+                icon="share"
+                text="WGSL Spec"
+                href="https://www.w3.org/TR/WGSL/"
+                variant="outlined"
+                target="_blank"
+            />
+            <AnchorButton
+                icon="git-new-branch"
+                text="View Source"
+                href="https://github.com/Athenodoros/wgsl-playground"
+                variant="outlined"
+                target="_blank"
+            />
+            <HelpNavbarButton />
+        </Navbar.Group>
+    </Navbar>
+);
 
 const HelpNavbarButton: React.FC = () => {
     const [instructionsOpen, setInstructionsOpen] = useState(false);
